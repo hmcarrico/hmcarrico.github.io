37cb394c016f18675e9a5866b2175649ad6f9202
Hunter Carrico <hmc.carrico@gmail.com>
2019-08-01 15:13:08 -0700

    build folder push

---
+++ build/precache-manifest.ee08101c2748b953ba8b4af63c3a59b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f09892f805f6c5d9b88ac737922c14",
+    "url": "/alien-review/index.html"
+  },
+  {
+    "revision": "d2f071d93e6a16763ccd",
+    "url": "/alien-review/static/css/main.87b4e2a9.chunk.css"
+  },
+  {
+    "revision": "9e4b3e62e98ee94b9636",
+    "url": "/alien-review/static/js/2.e186b6a4.chunk.js"
+  },
+  {
+    "revision": "d2f071d93e6a16763ccd",
+    "url": "/alien-review/static/js/main.5735c87f.chunk.js"
+  },
+  {
+    "revision": "f460abd9b785516cd183",
+    "url": "/alien-review/static/js/runtime~main.7f9b9bf8.js"
+  }
+]);
